fe2621b3c7ad35b5f2dca33479358941bb206d6b
Michael Hardin <hardinm78@gmail.com>
2017-11-26 08:48:17 -0600

    L5

--- app/src/main/java/com/mikehardinapps/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/mikehardinapps/swoosh/LeagueActivity.kt
@@ -1,6 +1,8 @@
 package com.mikehardinapps.swoosh
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 
 class LeagueActivity : BaseActivity() {
 
@@ -8,4 +10,12 @@ class LeagueActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
+
+    fun leagueNextClicked(view: View){
+
+        val skillActivity = Intent(this,SkillActivity::class.java)
+        startActivity(skillActivity)
+
+    }
+
 }
